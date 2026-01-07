fc25905a8b8027b8dd51110ef93b3b0867b8a5c4
Afif Al Saad <afifalsaad.edu@gmail.com>
2026-01-07 23:59:10 +0600

    solved ping pong problem

--- index.js
+++ index.js
@@ -102,12 +102,6 @@ const theWord = capitalizeWord(
 // console.log(theWord);
 
 // Problem 9: Find the Factorial of a Number
-// Write a function that calculates the factorial of a number using a loop.
-
-// Example:
-
-// Input: 5
-// Output: 120
 
 function factorialOfaNumber(num) {
   let res = 1;
@@ -118,17 +112,22 @@ function factorialOfaNumber(num) {
 }
 
 const factorial = factorialOfaNumber(6);
-console.log(factorial);
+// console.log(factorial);
 
 // Problem 10: PingPong Challenge
-// Write a function that prints numbers from 1 to 20.
 
-// Example:
+function pingPong() {
+  for (i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("Ping Pong");
+    } else if (i % 3 === 0) {
+      console.log("ping");
+    } else if (i % 5 === 0) {
+      console.log("Pong");
+    } else {
+      console.log(i);
+    }
+  }
+}
 
-// For multiples of 3, print "Ping"
-// For multiples of 5, print "Pong"
-// For multiples of both 3 and 5, print "PingPong"
-// If the number is not a multiple of 3 or 5, print the number itself
-// Example Output:
-
-// 1, 2, Ping, 4, Pong, Ping, 7, 8, Ping, Pong, 11, Ping, 13, 14, PingPong, 16 …..
+pingPong();
